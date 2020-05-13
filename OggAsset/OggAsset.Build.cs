db1358e86ec33c62616fb8c44e78025c2caf187e
Cygon <cygon@nuclex.org>
2020-05-13 10:31:09 +0200

    Removed uplugin and added module build rules

---
+++ OggAsset/OggAsset.Build.cs
@@ -0,0 +1,25 @@
+using UnrealBuildTool;
+
+public class OggAsset : ModuleRules {
+
+  public OggAsset(ReadOnlyTargetRules Target) : base(Target) {
+    PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+    PublicDependencyModuleNames.AddRange(
+      new string[] {
+        "Core", "CoreUObject", "Engine"
+      }
+    );
+
+    PublicDefinitions.Add("WITH_OGGVORBIS");
+
+    // Uncomment if you are using Slate UI
+    // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+    // Uncomment if you are using online features
+    // PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+    // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+  }
+
+}
