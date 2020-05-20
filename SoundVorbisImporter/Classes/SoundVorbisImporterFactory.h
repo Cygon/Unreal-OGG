b391f39504c35274032ff2f48603048b235065c6
Cygon <cygon@nuclex.org>
2020-05-20 23:32:12 +0200

    SoundVorbisImporterFactory now follows SoundModImporter design

--- SoundVorbisImporter/Classes/SoundVorbisImporterFactory.h
+++ SoundVorbisImporter/Classes/SoundVorbisImporterFactory.h
@@ -1,23 +1,65 @@
-// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+#pragma region MIT License
+/*
+OggAsset Library
+Copyright (C) 2017-2018 Scott Bishel
+Enhancements copyright (C) 2020 Markus Ewald
 
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+#pragma endregion // MIT License
 #pragma once
 
-#include "Factories/Factory.h"
-#include "OggAssetFactory.generated.h"
+#include <CoreMinimal.h>
+#include <Factories/Factory.h>
 
+#include <SoundVorbis.h>
 
-/**
- * Implements a factory for UMidiAsset objects.
- */
-UCLASS(hidecategories=Object)
-class UOggAssetFactory
-	: public UFactory
-{
+#include "SoundVorbisImporterFactory.generated.h"
+
+// --------------------------------------------------------------------------------------------- //
+
+/// Factory that imports Ogg Vorbis files and produces Vorbis audio assets
+UCLASS(Hidecategories=Object)
+class USoundVorbisImporterFactory : public UFactory {
+
 	GENERATED_UCLASS_BODY()
 
-public:
+	//
+	// UFactory implementation
+	//
 
-	// UFactory Interface
+  /// Creates a new SoundVorbis asset by importing it from binary data in a buffer
+	/// @remarks
+	///   Just like the bloaty factory class, this method drowns in arguments and
+	///   they're not even documented, so I have no idea what half of them are...
+  public: virtual UObject *FactoryCreateBinary(
+		UClass *class_,
+		UObject *inParent,
+		FName name,
+		EObjectFlags flags,
+		UObject *context,
+		const TCHAR *type,
+		const uint8 *&buffer,
+		const uint8 *bufferEnd,
+		FFeedbackContext *warn
+	) override;
 
-	virtual UObject* FactoryCreateBinary(UClass* Class, UObject* InParent, FName Name, EObjectFlags Flags, UObject* Context, const TCHAR* Type, const uint8*& Buffer, const uint8* BufferEnd, FFeedbackContext* Warn) override;
 };
+
+// --------------------------------------------------------------------------------------------- //
--- SoundVorbisImporter/Private/SoundVorbisImporterFactory.cpp
+++ SoundVorbisImporter/Private/SoundVorbisImporterFactory.cpp
@@ -1,31 +1,70 @@
-// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+#pragma region MIT License
+/*
+OggAsset Library
+Copyright (C) 2017-2018 Scott Bishel
+Enhancements copyright (C) 2020 Markus Ewald
 
-#include "OggAssetEditorPrivatePCH.h"
-#include "OggAssetFactory.h"
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-#include "../OggAsset/Public/SoundProcessingLibrary.h"
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
 
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+#pragma endregion // MIT License
 
-/* UOggAssetFactory structors
- *****************************************************************************/
+#include "SoundVorbisImporterFactory.h"
 
-UOggAssetFactory::UOggAssetFactory( const FObjectInitializer& ObjectInitializer )
-	: Super(ObjectInitializer)
-{
-	Formats.Add(FString(TEXT("ogg;")) + NSLOCTEXT("UOggAssetFactory", "Sound", "OGG File").ToString());
-	SupportedClass = USoundWave::StaticClass();
+//#include "../OggAsset/Public/SoundProcessingLibrary.h"
+
+// --------------------------------------------------------------------------------------------- //
+
+USoundVorbisImporterFactory::USoundVorbisImporterFactory(
+	const FObjectInitializer &objectInitializer
+) :
+	Super(objectInitializer) {
+
 	bCreateNew = false;
+	SupportedClass = USoundVorbis::StaticClass();
+
 	bEditorImport = true;
 
+	Formats.Add(
+		FString(TEXT("ogg;")) +
+		NSLOCTEXT("USoundVorbisImporterFactory", "Sound", "OGG Vorbis Audio").ToString()
+	);
 }
 
+// --------------------------------------------------------------------------------------------- //
 
-/* UFactory overrides
- *****************************************************************************/
+UObject *USoundVorbisImporterFactory::FactoryCreateBinary(
+	UClass *class_,
+	UObject *inParent,
+	FName name,
+	EObjectFlags flags,
+	UObject *context,
+	const TCHAR *type,
+	const uint8 *&buffer,
+	const uint8 *bufferEnd,
+	FFeedbackContext *warn
+) {
+	return nullptr;
+	/*
+	// If the sound already exists, look it up so we can reuse the import settings
+	USoundVorbis *existingSound = FindObject<USoundVorbis>(inParent, *name.ToString());
 
-UObject* UOggAssetFactory::FactoryCreateBinary(UClass* Class, UObject* InParent, FName Name, EObjectFlags Flags, UObject* Context, const TCHAR* Type, const uint8*& Buffer, const uint8* BufferEnd, FFeedbackContext* Warn)
-{
-	UOggAsset* OggAsset = nullptr;
+	UOggAsset *oggAsset = nullptr;
 	TArray<uint8> data;
 
 	if (FFileHelper::LoadFileToArray(data, *CurrentFilename))
@@ -36,4 +75,5 @@ UObject* UOggAssetFactory::FactoryCreateBinary(UClass* Class, UObject* InParent,
 	}
 
 	return OggAsset;
+	*/
 }
